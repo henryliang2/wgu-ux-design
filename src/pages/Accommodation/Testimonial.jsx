0bca0c163f084a6fcc679171b2c9c3daefc7206d
Henry Liang <henryliang@alumni.ubc.ca>
2024-05-25 21:05:07 -0700

    fix testimonial spelling

--- src/pages/Accommodation/Testimonial.jsx
+++ src/pages/Accommodation/Testimonial.jsx
@@ -2,7 +2,7 @@ const Testimonial = () => {
 
   return (
     <div className="testimonial">
-      <p className="testimonial__quote">“My husband and I had the most enjoyable time at the Grand Tahitian Hotel. We can’t wait to come back!”</p>
+      <p className="testimonial__quote">“My husband and I had the most enjoyable time at the Grand Tanitian Hotel. We can’t wait to come back!”</p>
       <p className="testimonial__name">- Clara H, London, UK</p>
     </div>
   )
